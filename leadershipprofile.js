33ab25ccf72dda79d9748aae0732975e04b26f6c
daz2023 <125355767+daz2023@users.noreply.github.com>
2024-02-21 10:31:46 +0000

    Update leadershipprofile.js

--- leadershipprofile.js
+++ leadershipprofile.js
@@ -200,8 +200,34 @@ const data = {
 labels: labels,
 datasets: [{
 label: 'Score Percentage',
-backgroundColor: 'rgba(0, 123, 255, 0.5)',
-borderColor: 'rgba(0, 123, 255, 1)',
+ backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(201, 203, 207, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)',
+ 'rgb(153, 102, 255)',
+ 'rgb(153, 102, 255)',
+ 'rgb(153, 102, 255)',
+ 'rgb(153, 102, 255)',
+ 'rgb(153, 102, 255)'
+    ],
 borderWidth: 1,
 data: dataPoints 
 }]
@@ -236,7 +262,4 @@ questionnaireDiv.addEventListener('change', function(event) {
 if (event.target && event.target.matches('input[type="radio"].response-option')) {
  handleNextQuestionAutomatically();
 }
-});
-nextBtn.addEventListener('click', handleNextButton);
-displayQuestion(currentQuestionIndex);
 });
